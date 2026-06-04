1870f3132025f851cd2552d7605ebba62a699b33
Soham Agarwal <agarwalsoham993@gmail.com>
2026-06-04 10:32:06 +0530

    server content type and hot reload

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ const http = require("http");   // ReferenceError: http is not defined
 // .statusCode is the status code of the response
 // .writeHead is the function that writes the headers of the response to the client
 // .on is the function that listens for events on the request
-// .listen is the function that starts the server
-// .close is the function that stops the server
-// .emit is the function that emits an event on the request
-
 
 
 const server = http.createServer((req, res) => {
-    console.log("request for: ", req.url)
-    res.end("Hello World")
+    console.log("request for: ", req.url)                   // logs in my console
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('this is the body \n');
+    res.write('this is body 2');
+    res.end();
 })
+
+// .listen is the function that starts the server
+// .close is the function that stops the server
+// .emit is the function that emits an event on the request
 
 server.listen(3000, () => console.log("Server listening on port 3000"))
